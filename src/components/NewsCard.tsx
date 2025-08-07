ec229176b0a36043cd9ddbfdaed40fca412b6892
Matt Hemsing <mhemsing@us.ibm.com>
2025-08-06 20:51:25 -0700

    Update NewsCard.tsx

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -201,7 +201,7 @@ export default function NewsCard({
                     🤓
                   </span>
                   <div
-                    className={`absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-900 text-white text-xs rounded transition-opacity duration-200 pointer-events-none whitespace-nowrap z-50 ${
+                    className={`absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-900 text-white text-xs rounded transition-opacity duration-200 pointer-events-none whitespace-nowrap z-50 hidden lg:block ${
                       showTooltip ? 'opacity-100' : 'opacity-0'
                     }`}>
                     Tell me more!
